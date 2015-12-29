9550fd577440959ca4bdfe0eea6ccd0428571cfc
Josh Hubbard <jhubbardsf@gmail.com>
2015-12-29 16:45:52 -0500

    Reverting.

--- db/migrate/20151229211011_add_minimum_to_styles.rb
+++
@@ -1,5 +0,0 @@
-class AddMinimumToStyles < ActiveRecord::Migration
-  def change
-    add_column :styles, :minimum, :decimal
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221005808) do
+ActiveRecord::Schema.define(version: 20151229211011) do
 
   create_table "clearance_batches", force: :cascade do |t|
     t.datetime "created_at"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151221005808) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "minimum"
   end
 
 end
